deba64ba3817ac15727326ec24b45d566d767bb8
Rocio Philipps <rocio.philipps@curateur.gouv.qc.ca>
2021-10-11 16:42:02 -0400

    End of section 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,16 +1,14 @@
 using System;
 using System.Collections.Generic;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -20,12 +18,6 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-
-        /*
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }*/
+        public ICollection<PhotoDto> Photos { get; set; }
     }
 }
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
