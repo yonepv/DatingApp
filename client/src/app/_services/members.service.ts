7c8c1a9ab53a74254eb792c0d0d3f484aa6f0f69
Rocio Philipps <rocio.philipps@curateur.gouv.qc.ca>
2021-11-13 08:01:29 -0500

    End of section 9

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,19 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_modules/member';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization:
-      //'Bearer' + JSON.parse(localStorage.getItem('user')).token,
-      //gives an error because of nullable type is not assignable to string type. localStorage.getString from null is false.
-      //assert is will not be null (one of the two solutions proposed by following link)
-      //see https://newbedev.com/argument-of-type-string-null-is-not-assignable-to-parameter-of-type-string-type-null-is-not-assignable-to-type-string-local-storage-code-example
-      'Bearer ' + JSON.parse(localStorage.getItem('user')!).token,
-  }),
-};
-
 @Injectable({
   providedIn: 'root',
 })
@@ -23,13 +11,10 @@ export class MembersService {
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(
-      this.baseUrl + 'users/' + username,
-      httpOptions
-    );
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
